7ba98322b9a71f27900806e339ee5dcc6cf413b6
Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
2016-03-29 09:42:46 -0600

    webpack build config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// this will build and serve the examples
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  debug: true,
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, 'examples'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'react-bootstrap-table': path.resolve(__dirname, './src')
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }, {
+        test: /\.css$/, loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+
+  plugins: [
+
+  ]
+};
